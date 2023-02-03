265f2e481e74e0a1427a44cb81dc0c2fa673a015
Ogwo Chinaza <ogwo.chinaza@yahoo.com>
2023-02-03 17:25:25 +0100

    done with task

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,16 +1,18 @@
 const Product = require("../models/Product");
+const Stock = require("../models/Stock");
 const Joi = require("joi");
 
 module.exports.get_products = async (req, res) => {
   const perPage = req.query.perPage || 5;
   const page = req.query.page || 1;
   const query = {
-    fullName: { $regex: req.query.name?.trim() ?? "", $options: "i" },
+    name: { $regex: req.query.name?.trim() ?? "", $options: "i" },
   };
   try {
     const products = await Product.find(query)
       .skip(perPage * page - perPage)
-      .limit(perPage);
+      .limit(perPage)
+      .populate("stocks");
 
     const count = await Product.count();
 
@@ -31,10 +33,75 @@ module.exports.get_products = async (req, res) => {
   }
 };
 
+module.exports.get_stocks = async (req, res) => {
+  const perPage = req.query.perPage || 5;
+  const page = req.query.page || 1;
+  const query = {
+    $and: [
+      { quantity: { $gt: req.query.min ?? 0 } },
+      { quantity: { $lt: req.query.max ?? Number.MAX_VALUE } },
+    ],
+  };
+  try {
+    const stocks = await Stock.find(query)
+      .skip(perPage * page - perPage)
+      .limit(perPage);
+
+    const count = await Stock.count();
+
+    return res.send({
+      success: true,
+      message: "Stocks fetched successfully",
+      currentPage: page,
+      perPage,
+      totalPages: Math.ceil(count / perPage),
+      totalResults: count,
+      data: stocks,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports.get_product_stocks = async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const product = await Product.findById(productId).populate("stocks");
+
+    if (!product) {
+      return res.status(404).send({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    const min = req.query.min || 0;
+    const max = req.query.max || Number.MAX_VALUE;
+
+    const stocks = product.stocks.filter(
+      (stock) => stock.quantity > min && stock.quantity < max
+    );
+
+    return res.send({
+      success: true,
+      message: "Stocks fetched successfully",
+      data: stocks,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports.get_product = async (req, res) => {
   const { productId } = req.params;
   try {
-    const product = await Product.findById(productId);
+    const product = await Product.findById(productId).populate("stocks");
 
     if (!product) {
       return res.status(404).send({
@@ -164,6 +231,58 @@ module.exports.update_product = async (req, res) => {
   }
 };
 
+module.exports.add_stock = async (req, res) => {
+  const schema = Joi.object().keys({
+    quantity: Joi.number().min(1).required().messages({
+      "any.required": "Stock quantity must be more than 0",
+    }),
+    productId: Joi.string().required().messages({
+      "any.required": "Product ID must be provided",
+    }),
+  });
+
+  const result = schema.validate(req.body);
+
+  if (result.error) {
+    return res.status(400).send({
+      success: false,
+      message: result.error.details[0].message,
+    });
+  }
+
+  try {
+    const product = await Product.findById(req.body.productId);
+
+    if (!product) {
+      return res.status(404).send({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    const stock = await Stock.create({
+      batchId: Math.random().toString(36).slice(2, 8).toUpperCase(),
+      quantity: req.body.quantity,
+    });
+
+    const stocks = product.stocks;
+    stocks.push(stock);
+    product.stocks = stocks;
+    await product.save();
+
+    return res.json({
+      success: true,
+      message: "Stock update successful",
+      data: product,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports.delete_product = async (req, res) => {
   const { productId } = req.params;
   try {
@@ -172,6 +291,55 @@ module.exports.delete_product = async (req, res) => {
     return res.json({
       success: true,
       message: "Product deleted!",
+    });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports.delete_stock = async (req, res) => {
+  const schema = Joi.object().keys({
+    productId: Joi.string().required().messages({
+      "any.required": "Product ID must be provided",
+    }),
+    stockId: Joi.string().required().messages({
+      "any.required": "Stock ID must be provided",
+    }),
+  });
+
+  const result = schema.validate(req.body);
+
+  if (result.error) {
+    return res.status(400).send({
+      success: false,
+      message: result.error.details[0].message,
+    });
+  }
+  try {
+    let product = await Product.findById(req.body.productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    if (!product.stocks.includes(req.body.stockId)) {
+      return res.status(404).json({
+        success: false,
+        message: "Stock does not belong to this product",
+      });
+    }
+
+    await Stock.deleteOne({ _id: req.body.stockId });
+
+    const stocks = product.stocks.filter((stock) => stock !== req.body.stockId);
+
+    product.stocks = stocks;
+
+    return res.json({
+      success: true,
+      message: "Stock deleted!",
     });
   } catch (error) {
     return res.status(400).json({ message: error.message });
--- routes/product.js
+++ routes/product.js
@@ -5,8 +5,15 @@ const controller = require("../controllers/product_controller");
 const router = Router();
 
 router.get("/products", requireAuth, controller.get_products);
+router.get("/stocks", requireAuth, controller.get_stocks);
+router.get(
+  "/product/stocks/:productId",
+  requireAuth,
+  controller.get_product_stocks
+);
 router.get("/product/:productId", requireAuth, controller.get_product);
 router.post("/product", requireAuth, controller.create_product);
+router.post("/product/stock", requireAuth, controller.add_stock);
 router.put(
   "/product/update/:productId",
   requireAuth,
@@ -17,5 +24,6 @@ router.delete(
   requireAuth,
   controller.delete_product
 );
+router.delete("/product/stock/delete/", requireAuth, controller.delete_stock);
 
 module.exports = router;
